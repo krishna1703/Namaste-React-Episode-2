28a07a5dd7dd0477096c9bb43865a55796653aca
krishna <krishnasharmag30@gmail.com>
2023-05-25 13:45:46 +0530

    Episode -2 Igniting our App

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 
 const parent = React.createElement("div", { id: "parent" },
     [React.createElement("div", { id: "child1", }, [
-        React.createElement("h1", {}, "i am h1 tag")
-        , React.createElement("h2", {}, "i am h2 tag")]),
+        React.createElement("h1", {}, "this is best react course ")
+        , React.createElement("h2", {}, "by Akshay bhaiya")]),
     React.createElement("div", { id: "child2", }, [
-        React.createElement("h1", {}, "i am h3 tag"),
-        React.createElement("h2", {}, "i am h4 tag")])]
+        React.createElement("h1", {}, "i am student of this course"),
+        React.createElement("h2", {}, " my self krishna kumar from kanpur up")])]
 )
 
 
